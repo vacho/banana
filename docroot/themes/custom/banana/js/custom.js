17062be92a8be696371e6803d3c9d0747b940bf8
vacho <vachisinio@gmail.com>
2020-06-09 16:17:06 -0400

    [config-theming] update programa display

--- docroot/themes/custom/banana/js/custom.js
+++ docroot/themes/custom/banana/js/custom.js
@@ -7,4 +7,16 @@ jQuery(document).ready(function($) {
   $(".node--type-programa h2:contains('Link Mac')").addClass("mac-icon");
   $(".node--type-programa h2:contains('Link Linux')").addClass("linux-icon");
   $(".node--type-programa div:has('> a.colorbox')").addClass("image-container");
+
+  var empty1 = $(".node--type-programa .block-field-blocknodeprogramafield-software-libre div:empty");
+  empty1.parent().remove();
+  var empty2 = $(".node--type-programa .block-field-blocknodeprogramafield-gratis div:empty");
+  empty2.parent().remove();
+  var empty3 = $(".node--type-programa .block-field-blocknodeprogramafield-demo div:empty");
+  empty3.parent().remove();
+  var empty4 = $(".node--type-programa .block-field-blocknodeprogramafield-online div:empty");
+  empty4.parent().remove();
+  var empty5 = $(".node--type-programa .block-field-blocknodeprogramafield-de-prueba div:empty");
+  empty5.parent().remove();
+
 });
